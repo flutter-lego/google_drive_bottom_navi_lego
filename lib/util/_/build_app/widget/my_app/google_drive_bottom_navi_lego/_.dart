484e724a876aff801424d9a2496af2c5e5e68e85
june lee <melodysdreamj@gmail.com>
2024-07-18 16:57:37 +0900

    Initial commit

---
+++ lib/util/_/build_app/widget/my_app/google_drive_bottom_navi_lego/_.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../../../../../../main.dart';
+
+// @CoverMyApp()
+// Widget Function() coverGoogleDriveBottomNaviLego(Widget Function() materialAppBuilder) {
+//   return () => materialAppBuilder();
+// }
